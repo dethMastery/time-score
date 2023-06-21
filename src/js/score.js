3a6da76ac8a8d063ecb8ccf066cfce8e066fe15a
Suphakit P <me@suphakit.net>
2023-06-21 21:26:12 +0700

    feat: score counting page :)

--- src/js/score.js
+++ src/js/score.js
@@ -10,14 +10,30 @@ let scoreSetup = [0, 0, 0, 0, 0];
 
 if (localStorage.getItem("score") == null) {
   for (let i = 0; i < 5; i++) {
-    teamScore[i].innerHTML = `<h1>0</h1>`;
+    teamScore[i].innerHTML = `<h1 class="id${i}">0</h1>`;
   }
 
   localStorage.setItem("score", JSON.stringify(scoreSetup));
 } else {
-  let scoring = JSON.parse(localStorage.getItem('score'))
+  let scoring = JSON.parse(localStorage.getItem("score"));
 
   for (let i = 0; i < 5; i++) {
-    teamScore[i].innerHTML = `<h1>${scoring[i]}</h1>`;
+    teamScore[i].innerHTML = `<h1 class="id${i}">${scoring[i]}</h1>`;
+  }
+}
+
+function add(id) {
+  let score = document.querySelector(`.id${id}`).innerHTML
+
+  document.querySelector(`.id${id}`).innerHTML = parseInt(score) + 1
+}
+
+function del(id) {
+  let score = document.querySelector(`.id${id}`).innerHTML
+
+  if (score - 1 <= 0) {
+    document.querySelector(`.id${id}`).innerHTML = 0
+  } else {
+    document.querySelector(`.id${id}`).innerHTML = parseInt(score) - 1
   }
 }
